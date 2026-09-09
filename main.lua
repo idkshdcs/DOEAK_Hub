96e33a8adff2bc05fbb48d5b8256be27a50338e9
idkshdcs <anhphatp599@gmail.com>
2026-09-09 21:41:43 +0700

    Refactor main.lua for improved readability

--- main.lua
+++ main.lua
@@ -6,8 +6,8 @@ print("🐉 Loading doeak hub...")
 
 -- Load các module
 local loadSuccess, loadErr = pcall(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/idkshdcs/DOEAK_Hub/main/orion.lua"))()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/idkshdcs/DOEAK_Hub/main/auto_farm.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/idkshdcs/DOEAK_Hub/main/orion.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/idkshdcs/DOEAK_Hub/main/auto_farm.lua"))()
 end)
 
 if not loadSuccess then
